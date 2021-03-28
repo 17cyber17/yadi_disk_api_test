c889645792dc8519b048f87683db69d22be6a73e
17cyber17 <17cyber17@gmail.com>
2021-03-28 11:31:44 +0700

    Реализован класс и основные функции для работы c API яндекс диска

--- yadi_disk_api.py
+++ yadi_disk_api.py
@@ -1,5 +1,35 @@
 import requests
-from requests_oauthlib import OAuth1
-headers = {'Authorization': 'AgAAAAARY09qAADLWzX1TShKB0zKjjC7hLBzxt4'}
-res = requests.put('https://cloud-api.yandex.net/v1/disk/resources?path=QA', headers=headers)
-print(res)
+
+class API():
+    def __init__(self):
+        self.base_url = 'https://cloud-api.yandex.net/v1/disk'
+        self.headers = {'Authorization': 'AgAAAAARY09qAADLWzX1TShKB0zKjjC7hLBzxt4'}
+
+    def create_folder(self, path_to_folder):
+        res = requests.put(f'{self.base_url}/resources?path={path_to_folder}', headers=self.headers)
+        print(res)
+
+    def create_copy_file_or_folder(self, path_to_the_copied_resource, path_to_the_created_resource):
+        res = requests.post(f'{self.base_url}/resources/copy?from={path_to_the_copied_resource}&path=\
+                                                                {path_to_the_created_resource}', headers=self.headers)
+        print(res)
+
+    def move_file_or_folder(self, path_resource_move, path_created_resource):
+        res = requests.post(f'{self.base_url}/resources/move?from={path_resource_move}&path=\
+                                                                        {path_created_resource}', headers=self.headers)
+        print(res)
+
+    def delete_file_or_folder(self, path_file_or_folder):
+        res = requests.delete(f'{self.base_url}/resources?path={path_file_or_folder}', headers=self.headers)
+        print(res)
+
+    def publish_resource(self, path_file_or_folder):
+        res = requests.put(f'{self.base_url}/resources/publish?path={path_file_or_folder}', headers=self.headers)
+        print(res)
+
+    def unpublish_resource(self, path_file_or_folder):
+        res = requests.put(f'{self.base_url}/resources/unpublish?path={path_file_or_folder}', headers=self.headers)
+        print(res)
+
+    def upload_url(self, path_created_resource, url):
+        res = requests.post(f'{self.base_url}/resources/upload?path={path_created_resource}&url={url}', headers=self.headers )
