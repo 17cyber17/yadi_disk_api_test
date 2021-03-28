c673336d2ec6b9adc2715ba6badf6a3303e0535b
17cyber17 <17cyber17@gmail.com>
2021-03-28 14:14:56 +0700

    Добавленны локаторы для страницы логина

--- pages/locators.py
+++ pages/locators.py
@@ -9,3 +9,8 @@ class BasePageLocators():
     JOURNAL_LINK = (By.CSS_SELECTOR, '[href="/client/journal"]')
     ATTACH_LINK = (By.CSS_SELECTOR, '[href="/client/attach"]')
     TRASH_LINK = (By.CSS_SELECTOR, '[href="/client/trash"]')
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, '#passp-field-login')
+    PASSWORD_FORM = (By.CSS_SELECTOR, '#passp-field-passwd')
+    LOGIN_BTN = (By.CSS_SELECTOR, '[class = "passp-button passp-sign-in-button"] > .Button2')
