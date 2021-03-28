62d233001b17fad76acdd6783ee2989f8415a251
17cyber17 <17cyber17@gmail.com>
2021-03-28 14:34:33 +0700

    Изменены локаторы для страницы логина

--- pages/locators.py
+++ pages/locators.py
@@ -11,6 +11,7 @@ class BasePageLocators():
     TRASH_LINK = (By.CSS_SELECTOR, '[href="/client/trash"]')
 
 class LoginPageLocators():
-    LOGIN_FORM = (By.CSS_SELECTOR, '#passp-field-login')
-    PASSWORD_FORM = (By.CSS_SELECTOR, '#passp-field-passwd')
+    LOGIN_FORM = (By.CSS_SELECTOR, '.passp-auth-content')
+    LOGIN_INPUT = (By.CSS_SELECTOR, '#passp-field-login')
+    PASSWORD_INPUT = (By.CSS_SELECTOR, '#passp-field-passwd')
     LOGIN_BTN = (By.CSS_SELECTOR, '[class = "passp-button passp-sign-in-button"] > .Button2')
