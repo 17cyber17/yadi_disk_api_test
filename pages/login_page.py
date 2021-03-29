92340cc3e531086f223e0175eadbd1b7b437e38f
17cyber17 <17cyber17@gmail.com>
2021-03-29 12:05:56 +0700

    Исправлен поиск кнопки "Войти" после ввода логина

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,16 +1,22 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
-from selenium import webdriver
+import time
 
 class LoginPage(BasePage):
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "No login form"
 
+    def should_be_login(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_INPUT), "No"
+
     def login_user(self, login, password):
-        login_input = self.is_element_present(*LoginPageLocators.LOGIN_INPUT)
-        login_btn = self.is_element_present(*LoginPageLocators.LOGIN_BTN)
+        login_input = self.browser.find_element(*LoginPageLocators.LOGIN_INPUT)
+        login_btn = self.browser.find_element(*LoginPageLocators.LOGIN_BTN)
         login_input.send_keys(login)
         login_btn.click()
-        password_input = self.is_element_present(*LoginPageLocators.PASSWORD_INPUT)
+        password_input = self.browser.find_element(*LoginPageLocators.PASSWORD_INPUT)
         password_input.send_keys(password)
+        # Кнопка после заполнения логина меняет свое положение но локатор у нее тот же
+        login_btn2 = self.browser.find_element(*LoginPageLocators.LOGIN_BTN)
+        login_btn2.click()
 
