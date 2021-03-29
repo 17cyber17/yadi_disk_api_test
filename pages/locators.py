47c3438680ae3bb6a02068e175298559781be0c6
17cyber17 <17cyber17@gmail.com>
2021-03-29 17:22:16 +0700

    Добавлена функция поиска папки\файла на странице по передеваемому в нее имени

--- pages/locators.py
+++ pages/locators.py
@@ -9,9 +9,13 @@ class BasePageLocators():
     JOURNAL_LINK = (By.CSS_SELECTOR, '[href="/client/journal"]')
     ATTACH_LINK = (By.CSS_SELECTOR, '[href="/client/attach"]')
     TRASH_LINK = (By.CSS_SELECTOR, '[href="/client/trash"]')
+    def search_for_file_or_folder(self, name):
+        FOLDER_OR_FILE = (By.XPATH, f"//span[text()='{name}']")
+        return FOLDER_OR_FILE
 
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, '.passp-auth-content')
     LOGIN_INPUT = (By.CSS_SELECTOR, '#passp-field-login')
     PASSWORD_INPUT = (By.CSS_SELECTOR, '#passp-field-passwd')
     LOGIN_BTN = (By.CSS_SELECTOR, '[class = "passp-button passp-sign-in-button"] > .Button2')
+
