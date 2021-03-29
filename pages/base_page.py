77ee32994a1672e5349932bc8a5db2d8967c0d03
17cyber17 <17cyber17@gmail.com>
2021-03-29 17:21:41 +0700

    Добавлена функция для проверки существования папки или файла на диске

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,9 +1,11 @@
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-from .locators import BasePageLocators
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from .locators import BasePageLocators
+import urllib.parse
 
 class BasePage():
     def __init__(self, browser, url, timeout=10):
@@ -40,6 +42,34 @@ class BasePage():
 
     def should_be_journal_link(self):
         assert self.is_element_present(*BasePageLocators.JOURNAL_LINK), "Journal link is not present"
+
+    def should_be_folder_or_file(self, path):
+        true_path = urllib.parse.unquote(path)
+        end_name = 0
+        beginning_name = 0
+        locator = BasePageLocators()
+
+        for i in true_path:
+            end_name += 1
+            if i == "/":
+                print(true_path[beginning_name:end_name - 1])
+                locator_folder_or_file = locator.search_for_file_or_folder(true_path[beginning_name:end_name - 1])
+                assert self.is_element_present(*locator_folder_or_file), f'There is no folder named "{true_path[beginning_name:end_name - 1]}"'
+                folder = self.browser.find_element(*locator_folder_or_file)
+                driver = self.browser
+                actionChains = ActionChains(driver)
+                actionChains.double_click(folder).perform()
+                beginning_name = end_name
+
+        if beginning_name == 0 and end_name != 0:
+            print(true_path)
+            locator_folder_or_file = locator.search_for_file_or_folder(true_path)
+            assert self.is_element_present(*locator_folder_or_file), f'There is no folder named "{true_path}"'
+        else:
+            if beginning_name != 0:
+                print(true_path[beginning_name:end_name])
+                locator_folder_or_file = locator.search_for_file_or_folder(true_path[beginning_name:end_name])
+                assert self.is_element_present(*locator_folder_or_file), f'There is no folder named "{true_path[beginning_name:end_name]}"'
 
     def go_to_resent_page(self):
         link = self.browser.find_element(*BasePageLocators.RECENT_LINK)
