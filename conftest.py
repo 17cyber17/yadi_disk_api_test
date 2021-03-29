30ad418b91e542f277f96def7919b7822191f3a9
17cyber17 <17cyber17@gmail.com>
2021-03-29 12:05:12 +0700

    Добавленна поддержка запуска сайта с разными языковыми версиями

--- conftest.py
+++ conftest.py
@@ -3,7 +3,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default=None,
+    parser.addoption('--language', action='store', default='ru',
                      help="Choose language")
 
 @pytest.fixture(scope="function")
