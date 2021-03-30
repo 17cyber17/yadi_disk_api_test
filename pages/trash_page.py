b506549c37ecc37b0759ecd68c272cb3fd6994ff
17cyber17 <17cyber17@gmail.com>
2021-03-30 15:34:30 +0700

    Добавлены тесты для корзины

---
+++ pages/trash_page.py
@@ -0,0 +1,5 @@
+from .base_page import BasePage
+
+class TrashPage(BasePage):
+    def __init__(self, *args, **kwargs):
+        super(TrashPage, self).__init__(*args, **kwargs)
---
+++ test_trash_page.py
@@ -0,0 +1,39 @@
+from .pages.login_page import LoginPage
+from .pages.trash_page import TrashPage
+from .yadi_disk_api import API
+import pytest
+import time
+
+class TestUserShared():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "https://disk.yandex.ru/client/trash"
+        login_page = LoginPage(browser, link)
+        login_page.open()
+        login = "cyber17.semyon@yandex.ru"
+        password = "321654987asdfghjz"
+        login_page.login_user(login, password)
+
+    @pytest.mark.skip
+    def test_delete_folder(self, browser):
+        link = "https://disk.yandex.ru/client/trash"
+        page = TrashPage(browser, link)
+        disk = API()
+        path_to_folder = "QA"
+        disk.create_folder(path_to_folder)
+        disk.delete_file_or_folder(path_to_folder)
+        page.should_be_folder_or_file(path_to_folder)
+        disk.empty_trash()
+
+    @pytest.mark.skip
+    def test_empty_trash(self, browser):
+        link = "https://disk.yandex.ru/client/trash"
+        page = TrashPage(browser, link)
+        disk = API()
+        path_to_folder = "QA"
+        disk.create_folder(path_to_folder)
+        disk.delete_file_or_folder(path_to_folder)
+        page.should_be_folder_or_file(path_to_folder)
+        disk.empty_trash()
+        time.sleep(1)
+        page.should_not_be_folder_or_file(path_to_folder)
