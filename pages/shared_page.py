34ac70dc9b4eb11201a6084da6ad7c6d50dfbf99
17cyber17 <17cyber17@gmail.com>
2021-03-30 14:08:28 +0700

    Реализованы тесты страницы общего доступа

---
+++ pages/shared_page.py
@@ -0,0 +1,5 @@
+from .base_page import BasePage
+
+class SharedPage(BasePage):
+    def __init__(self, *args, **kwargs):
+        super(SharedPage, self).__init__(*args, **kwargs)
---
+++ test_shared_page.py
@@ -0,0 +1,37 @@
+from .pages.login_page import LoginPage
+from .pages.shared_page import SharedPage
+from .yadi_disk_api import API
+import pytest
+
+class TestUserShared():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "https://disk.yandex.ru/client/published"
+        login_page = LoginPage(browser, link)
+        login_page.open()
+        login = "cyber17.semyon@yandex.ru"
+        password = "321654987asdfghjz"
+        login_page.login_user(login, password)
+
+    @pytest.mark.skip
+    def test_create_publish(self, browser):
+        link = "https://disk.yandex.ru/client/published"
+        page = SharedPage(browser, link)
+        disk = API()
+        path_to_folder = "QA"
+        disk.create_folder(path_to_folder)
+        disk.publish_resource(path_to_folder)
+        page.should_be_folder_or_file(path_to_folder)
+        disk.delete_file_or_folder(path_to_folder)
+
+    def test_create_unpublish(self, browser):
+        link = "https://disk.yandex.ru/client/published"
+        page = SharedPage(browser, link)
+        disk = API()
+        path_to_folder = "QA"
+        disk.create_folder(path_to_folder)
+        disk.publish_resource(path_to_folder)
+        page.should_be_folder_or_file(path_to_folder)
+        disk.unpublish_resource(path_to_folder)
+        page.should_not_be_folder_or_file(path_to_folder)
+        disk.delete_file_or_folder(path_to_folder)
