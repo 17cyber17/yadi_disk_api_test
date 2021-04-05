bc0c958e60e17f9f54da8ff5dbc65aeb8ba783a4
17cyber17 <17cyber17@gmail.com>
2021-04-05 14:34:09 +0700

    Добавлены методы проверки нахождения\отсутствия папки в корзине

--- pages/trash_page.py
+++ pages/trash_page.py
@@ -17,3 +17,31 @@ class TrashPage(BasePage):
 
         if beginning_name == 0 and end_name != 0:
             disk.delete_file_or_folder(path_to_folder)
+
+    def should_not_be_folder_in_trash(self, path_to_folder):
+        end_name = 0
+        beginning_name = 0
+        true_path = urllib.parse.unquote(path_to_folder)
+        for i in true_path:
+            end_name += 1
+            if i == "/":
+                self.should_not_be_folder_or_file(path_to_folder[0:end_name - 1])
+                beginning_name = end_name
+                break
+
+        if beginning_name == 0 and end_name != 0:
+            self.should_not_be_folder_or_file(path_to_folder)
+
+    def should_be_folder_in_trash(self, path_to_folder):
+        end_name = 0
+        beginning_name = 0
+        true_path = urllib.parse.unquote(path_to_folder)
+        for i in true_path:
+            end_name += 1
+            if i == "/":
+                self.should_be_folder_or_file(path_to_folder[0:end_name - 1])
+                beginning_name = end_name
+                break
+
+        if beginning_name == 0 and end_name != 0:
+            self.should_be_folder_or_file(path_to_folder)
