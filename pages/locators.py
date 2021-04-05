8e82140db47cb3a817dc6cfba44b4b4134c7b7c6
17cyber17 <17cyber17@gmail.com>
2021-04-05 14:02:42 +0700

    Удалены ненужные локаторы

--- pages/locators.py
+++ pages/locators.py
@@ -1,15 +1,6 @@
 from selenium.webdriver.common.by import By
 
 class BasePageLocators():
-    RECENT_LINK = (By.CSS_SELECTOR, '[href="/client/recent"]')
-    DISK_LINK = (By.CSS_SELECTOR, '#/disk')
-    PHOTO_LINK = (By.CSS_SELECTOR, '[href="/client/photo"]')
-    ALBUMS_LINK = (By.CSS_SELECTOR, '[href="/client/albums"]')
-    SHARED_LINK = (By.CSS_SELECTOR, '[href="/client/shared"]')
-    JOURNAL_LINK = (By.CSS_SELECTOR, '[href="/client/journal"]')
-    ATTACH_LINK = (By.CSS_SELECTOR, '[href="/client/attach"]')
-    TRASH_LINK = (By.CSS_SELECTOR, '[href="/client/trash"]')
-
     def search_for_file_or_folder(self, name):
         FOLDER_OR_FILE = (By.XPATH, f"//span[text()='{name}']")
         return FOLDER_OR_FILE
