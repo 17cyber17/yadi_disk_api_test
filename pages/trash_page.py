882dac66366956a04edc8bc88d28c12028c28e78
17cyber17 <17cyber17@gmail.com>
2021-04-05 14:05:29 +0700

    Добавлен метод удаления папки со всем содержимым.

--- pages/trash_page.py
+++ pages/trash_page.py
@@ -1,5 +1,19 @@
 from .base_page import BasePage
+from yadi_disk_api import API
+import urllib.parse
 
 class TrashPage(BasePage):
-    def __init__(self, *args, **kwargs):
-        super(TrashPage, self).__init__(*args, **kwargs)
+    def delete_full_folder(self, path_to_folder):
+        end_name = 0
+        beginning_name = 0
+        disk = API()
+        true_path = urllib.parse.unquote(path_to_folder)
+        for i in true_path:
+            end_name += 1
+            if i == "/":
+                disk.delete_file_or_folder(path_to_folder[0:end_name - 1])
+                beginning_name = end_name
+                break
+
+        if beginning_name == 0 and end_name != 0:
+            disk.delete_file_or_folder(path_to_folder)
