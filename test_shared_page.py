ca54a9f9559fee20dc027fa500a66372e9484ebc
17cyber17 <17cyber17@gmail.com>
2021-04-18 14:26:06 +0700

    Исправлены тесты

--- test_shared_page.py
+++ test_shared_page.py
@@ -3,7 +3,8 @@ from .pages.shared_page import SharedPage
 from .yadi_disk_api import API
 import pytest
 
-class TestUserShared():
+
+class TestUserShared:
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, browser):
         link = "https://disk.yandex.ru/client/published"
@@ -13,18 +14,20 @@ class TestUserShared():
         password = "321654987asdfghjz"
         login_page.login_user(login, password)
 
-    def test_create_publish(self, new_folder, browser):
+    def test_create_publish_root(self, new_folder, browser):
         link = "https://disk.yandex.ru/client/published"
         page = SharedPage(browser, link)
         disk = API()
-        disk.publish_resource(new_folder)
-        page.should_be_folder_or_file(new_folder)
+        path = page.root_directory(new_folder)
+        disk.publish_resource(path)
+        page.should_be_folder_or_file(path)
 
-    def test_create_unpublish(self, new_folder, browser):
+    def test_create_unpublish_root(self, new_folder, browser):
         link = "https://disk.yandex.ru/client/published"
         page = SharedPage(browser, link)
         disk = API()
-        disk.publish_resource(new_folder)
-        page.should_be_folder_or_file(new_folder)
-        disk.unpublish_resource(new_folder)
-        page.should_not_be_folder_or_file(new_folder)
+        path = page.root_directory(new_folder)
+        disk.publish_resource(path)
+        page.should_be_folder_or_file(path)
+        disk.unpublish_resource(path)
+        page.should_not_be_folder_or_file(path)
