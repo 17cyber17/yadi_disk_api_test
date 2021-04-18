553bb6f258b60ac0eaf48c73b21a8b8ec8cefeb2
17cyber17 <17cyber17@gmail.com>
2021-04-18 13:40:06 +0700

    Тесты пределаны на использование новых функций из BasePage вместо старых из TrashPage

--- test_trash_page.py
+++ test_trash_page.py
@@ -3,8 +3,9 @@ from .pages.trash_page import TrashPage
 from .yadi_disk_api import API
 import pytest
 
+
 # Когда добавялешь Trash в название почему то запускаются сразу все тесты
-class TestUser():
+class TestUser:
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, browser):
         link = "https://disk.yandex.ru/client/trash"
@@ -18,19 +19,19 @@ class TestUser():
         link = "https://disk.yandex.ru/client/trash"
         page = TrashPage(browser, link)
         page.delete_full_folder(new_folder)
-        page.should_be_folder_in_trash(new_folder)
+        # Проверяем только корень так как в папке нельзя просматривать вложения
+        page.should_be_root(new_folder)
 
     def test_empty_trash(self, new_folder, new_file, browser):
         link = "https://disk.yandex.ru/client/trash"
         page = TrashPage(browser, link)
         disk = API()
         page.delete_full_folder(new_folder)
-        page.should_be_folder_in_trash(new_folder)
+        page.should_be_root(new_folder)
         disk.delete_file_or_folder(new_file)
         page.should_be_folder_or_file(new_file)
         disk.empty_trash()
-        page.should_not_be_folder_in_trash(new_folder)
-        # Пришлось написать новую функцию для проверки папки в корзине так как в ней нельзя просматривать вложенния
+        page.should_not_be_root(new_folder)
         page.should_not_be_folder_or_file(new_file)
 
     def test_restore_resource_from_trash(self, new_folder, new_file, browser):
@@ -38,11 +39,11 @@ class TestUser():
         page = TrashPage(browser, link)
         disk = API()
         page.delete_full_folder(new_folder)
-        page.should_be_folder_in_trash(new_folder)
+        page.should_be_root(new_folder)
         disk.delete_file_or_folder(new_file)
         page.should_be_folder_or_file(new_file)
         path = page.path_to_trash()
         disk.restore_resource_from_trash(path[0])
-        page.should_not_be_folder_in_trash(new_folder)
+        page.should_not_be_root(new_folder)
         disk.restore_resource_from_trash(path[1])
         page.should_not_be_folder_or_file(new_file)
